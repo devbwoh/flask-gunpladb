cf4486096054576de5bfe84f9a62e82937c2b0ed
BW Oh <byoungwoo5@naver.com>
2021-01-23 01:55:27 +0000

    ❤️ Fix Korean characters (한글)

--- gunpla.py
+++ gunpla.py
@@ -1,12 +1,37 @@
-from flask import Flask
-
+from flask import Flask, jsonify
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'mysql1234'
+app.config['MYSQL_DB'] = 'gunpladb'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+app.config['JSON_SORT_KEYS'] = False
+db = MySQL(app)
+
 
 @app.route("/")
-def test():
+def index():
     return "성공!"
+
+
+@app.route("/mechanic", methods=["GET"])
+def mechanic():
+    query = 'select * from mechanic'
+    cur = db.connection.cursor()
+    cur.execute(query)
+    return jsonify(cur.fetchall())
+
+# @app.route("/mechanic", methods=["GET"])
+# def mechanic():
+#     query = 'select * from mechanic'
+#     cur = db.connection.cursor()
+#     cur.execute(query)
+#     r = Response(response=json.dumps(cur.fetchall()))
+#     r.headers["Content-Type"] = "application/json; charset=utf-8"
+#     return r
 
 
 if __name__ == "__main__":
